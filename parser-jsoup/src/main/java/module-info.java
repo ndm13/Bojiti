26da996c549f87aac4e02527e255d516f3ddff6a
ndm13 <ndm0113@gmail.com>
2018-03-15 11:37:34 -0400

    Efficiency and reliability tweaks  - `Backend` now has a default `Worker` factory method.  - `RegexBasedParser#finesse(...)` allows for relative resolution hints.  - `HTMLParser` delegates some resolutions to `RegexBasedParser`  SPECIFICALLY.  This requires another export and require statement in  the necessary module-info files.  - `InMemoryBackend` splits different classes of discovered URIs.  - Optimized deobfuscation regexes in `TextParser`.  - Safety improvements in `URLConnectionDownloader`.  - Improved thread monitoring in `Worker`: should decrease chance of  deadlock.  - URI list in `WorkerTest` is now sorted.

--- parser-jsoup/src/main/java/module-info.java
+++ parser-jsoup/src/main/java/module-info.java
@@ -3,6 +3,7 @@ import net.miscfolder.bojiti.parser.jsoup.HTMLParser;
 
 module parser.jsoup{
 	requires core;
+	requires parser.regex;
 	requires jsoup;
 
 	provides net.miscfolder.bojiti.parser.Parser with
